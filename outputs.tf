67997fd600731f15ae5fd76691804443631a8659
Vitek Urbanec <vitek.urbanec@hatch.live>
2020-07-23 16:57:17 +0300

    fixed typo

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,7 @@ output "target_policy_arn" {
 }
 
 output "target_role_arn" {
-  value = module.target_setup.aws_iam_role.target_role.arn
+  value = module.target_setup.target_role_arn
 }
 # output "user_access_key_encrypted" {
 #   value = module.access_setup.user_access_key_encrypted
