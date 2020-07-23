0c842adb43aa7b795f311b119f03a4ef625ef0b7
Vitek Urbanec <vitek.urbanec@hatch.live>
2020-07-23 16:54:13 +0300

    added target role arn output

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,10 @@
 output "target_policy_arn" {
   value = module.target_setup.target_policy_arn
 }
+
+output "target_role_arn" {
+  value = module.target_setup.aws_iam_role.target_role.arn
+}
 # output "user_access_key_encrypted" {
 #   value = module.access_setup.user_access_key_encrypted
 # }
--- target-setup/outputs.tf
+++ target-setup/outputs.tf
@@ -10,3 +10,7 @@ output "target_policy_arn" {
   # value = data.aws_iam_policy.target_policy_managed[0].arn ? aws_iam_policy.target_policy_new[0].arn : ""
   value = coalesce(join("", data.aws_iam_policy.target_policy_managed.*.arn), join("", aws_iam_policy.target_policy_new.*.arn))
 }
+
+output "target_role_arn" {
+  value = aws_iam_role.target_role.arn
+}
