6fb523a4881007a4b7dc7058adfc22c6ecadd09a
Vitek Urbanec <vitek.urbanec@hatch.live>
2020-07-23 16:43:41 +0300

    add target policy output

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@
 #   value = module.target_setup.target_policy_arn
 # }
 
-# output "target_policy_managed_arn" {
-#   value = module.target_setup.target_policy_arn
-# }
+output "target_policy_arn" {
+  value = module.target_setup.target_policy_arn
+}
 # output "user_access_key_encrypted" {
 #   value = module.access_setup.user_access_key_encrypted
 # }
