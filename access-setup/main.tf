8398420be6861e0128e0ae4360e142f3086d2f24
Vitek Urbanec <vitek.urbanec@hatch.live>
2020-07-23 13:45:12 +0300

    cleanup

--- access-setup/main.tf
+++ access-setup/main.tf
@@ -31,59 +31,6 @@ resource "aws_iam_user_group_membership" "access_group_membership" {
   ]
 }
 
-# resource "aws_iam_policy" "allow_sts_policy_new" {
-
-#   count = var.using_managed_policy ? 0 : 1
-
-#   name        = var.allow_sts_policy_name
-#   path        = "/"
-#   description = "managed by terraform"
-
-#   policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": {
-#     "Effect": "Allow",
-#     "Action": "sts:AssumeRole",
-#     "Resource": "${var.target_policy_new_arn}"
-#   }
-# }
-# EOF
-# }
-
-# resource "aws_iam_policy" "allow_sts_policy_managed" {
-
-#   count = var.using_managed_policy ? 1 : 0
-
-#   name        = var.allow_sts_policy_name
-#   path        = "/"
-#   description = "managed by terraform"
-
-#   policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": {
-#     "Effect": "Allow",
-#     "Action": "sts:AssumeRole",
-#     "Resource": "${var.target_policy_managed_arn}"
-#   }
-# }
-# EOF
-# }
-
-# resource "aws_iam_group_policy_attachment" "policy_attachment_new" {
-#     count = var.using_managed_policy ? 0 : 1
-
-#   group      = aws_iam_group.access_group.name
-#   policy_arn = aws_iam_policy.allow_sts_policy_new[0].arn
-# }
-
-# resource "aws_iam_group_policy_attachment" "policy_attachment_managed" {
-#     count = var.using_managed_policy ? 1 : 0
-
-#   group      = aws_iam_group.access_group.name
-#   policy_arn = aws_iam_policy.allow_sts_policy_managed[0].arn
-# }
 
 
 resource "aws_iam_policy" "allow_sts_policy" {
@@ -103,6 +50,8 @@ resource "aws_iam_policy" "allow_sts_policy" {
 }
 EOF
 }
+
+
 
 resource "aws_iam_group_policy_attachment" "policy_attachment" {
   group      = aws_iam_group.access_group.name
